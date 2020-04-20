9e5869ba6d45ffec85b3e6e14f7009b615090cc6
NicolasSteckel <44899523+NicolasSteckel@users.noreply.github.com>
2020-04-20 18:33:57 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,189 +2,182 @@
 #include <stdlib.h>
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
+//#include "map.h"
 
-#define LARGEUR_TILE 33  // hauteur et largeur des tiles.
-#define HAUTEUR_TILE 33
-
-#define NOMBRE_BLOCS_LARGEUR 15  // nombre a afficher en x et y
-#define NOMBRE_BLOCS_HAUTEUR 13
-
-const int FPS=60;
-
-void Afficher(SDL_Renderer* pRenderer, SDL_Texture* textuTil, char** table, int nombre_blocs_largeur, int nombre_blocs_hauteur,int xBase, int yBase);
+int main(int argc, char *argv[])
+{
+    SDL_Window *pWindow=NULL;           //Pointeur sur la fenêtre, ref d'un window win32
+    SDL_Renderer *pRenderer=NULL;       //Pointeur sur le Rendus, ref rendu encapsulé dans window
+    //SDL_Texture *pTexture=NULL;         //Pointeur sur la texture, qui possède l'image à afficher, ref image/texture/materiel
 
 
+    //Requis pour utiliser la SDL
+    if(SDL_Init(SDL_INIT_EVERYTHING)>=0)    //INIT_EVERYTHING = initialise l'audio, la vidéo, les contrôles,... cf; SDL wiki/API by name/SDL_Init
+    {
+        //succès, créer window
+        pWindow=SDL_CreateWindow("SDL_Application",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 780, SDL_WINDOW_SHOWN);//x,y,width,eight
 
-int main(int argc, char *argv[]) {
-    SDL_Window *pWindow=NULL;
-    SDL_Renderer *pRenderer=NULL;
+      //si succès, créer window et renderer
+        if(pWindow!=NULL)
+        {
+            pRenderer=SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_PRESENTVSYNC);
+           /* }
+            else
+            {*/
 
-
-    if(SDL_Init(SDL_INIT_EVERYTHING)>=0) {
-        pWindow = SDL_CreateWindow("TILESET_TEST", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, LARGEUR_TILE*NOMBRE_BLOCS_LARGEUR, HAUTEUR_TILE*NOMBRE_BLOCS_HAUTEUR, SDL_WINDOW_SHOWN);
-
-        if(pWindow!=NULL) {
-            pRenderer=SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_PRESENTVSYNC);
             SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
 
+            //------------------------------------------------------------------------------------------------//
+            //Tilemapping
+
+            #define LARGEUR_TILE 33  // hauteur et largeur des tiles.
+            #define HAUTEUR_TILE 33
+
+            #define NOMBRE_BLOCS_LARGEUR 15  // nombre a afficher en x et y
+            #define NOMBRE_BLOCS_HAUTEUR 13
 
             char* table[] = {
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
-                "000000000000000000000044400000",
-                "000000000000000000000000000000",
-                "000000000000000000000000000000",
-                "000000022222200000000000000000",
-                "000000000000000000000022222000",
-                "001100000000000000000000000000",
-                "001100000000000000000000000000",
-                "001100000000000003300000000000",
-                "001100000000000003300000000000",
-                "555555555555555555555555555555",
-                "555555555555555555555555555555"
-            };
 
-            unsigned long int nTempsActuel=0;
-            unsigned long int nTempsPrecedent=0;
-            int nDeltaTime = div(1000,FPS).quot;
-            int PixelX = 0;
-            int PixelY = 0;
-            int sens = 1;
+            "000000000000000",
+            "000000000000000",
+            "000000000000000",
+            "000000000000000",
+            "000000000333444",
+            "000000000000000",
+            "000000000000000",
+            "000000000000000",
+            "222200000000000",
+            "222200000000550",
+            "222200000000550",
+            "222200000000550",
+            "555555555555555"};
 
-            SDL_Surface *tileset = NULL;
-            SDL_Texture *textuTil = NULL;
-            SDL_Event event;
-            int continuer = 1, tempsPrecedent = 0, tempsActuel = 0;
-            int statut = EXIT_FAILURE;
 
-            tileset = IMG_Load("brick_tiles_1.bmp");
-            if (tileset == NULL) {
-                fprintf(stderr, "Erreur chargement image droite : %s", SDL_GetError());
-            }
-
-            textuTil = SDL_CreateTextureFromSurface(pRenderer, tileset);
-            if (textuTil == NULL) {
-                fprintf(stderr, "Erreur SDL_CreateTexturetil : %s", SDL_GetError());
-            }
-            SDL_FreeSurface(tileset);
-
-            Afficher(pRenderer, textuTil, table, NOMBRE_BLOCS_LARGEUR, NOMBRE_BLOCS_HAUTEUR,0,0);
-
-            // Scrolling remplace par les controles
-            /*while(continuer) {
-                SDL_PollEvent(&event);
-                switch(event.type) {
-                    case SDL_QUIT:
-                        continuer = 0;
-                        break;
-
-                    case SDL_KEYDOWN:
-                        switch(event.key.keysym.sym) {
-                            case SDLK_ESCAPE:
-                                continuer = 0;
-                                break;
-                        }
-                        break;
-                }
-
-                nTempsActuel = SDL_GetTicks();
-                if (nTempsActuel > nTempsPrecedent + nDeltaTime) {          //On effectue l'affichage à chaque frame.
-
-                    if(sens == 1) {
-                        PixelX++;
-                    } else {
-                        PixelX--;
-                    }
-
-                    if((PixelX >= ((strlen(table[1]) - NOMBRE_BLOCS_LARGEUR) * LARGEUR_TILE)) || (PixelX <= 0)) {
-                        sens = -sens;
-                    }
-
-                    Afficher(pRenderer, textuTil, table, NOMBRE_BLOCS_LARGEUR, NOMBRE_BLOCS_HAUTEUR,PixelX,PixelY);
-                    nTempsPrecedent = nTempsActuel;
-
-                }
-
-                SDL_RenderPresent(pRenderer);
-            }*/
-
-            //Contrôles ZQSD
-            while(continuer)
+            void Afficher(SDL_Renderer* pRenderer, SDL_Texture* textuTil,
+                    char** table, int nombre_blocs_largeur, int nombre_blocs_hauteur)
             {
-                SDL_PollEvent(&event);
-                switch(event.type)
+                int i, j;
+                SDL_Rect Rect_dest;
+                SDL_Rect Rect_source;
+                Rect_source.w = LARGEUR_TILE;
+                Rect_dest.w   = LARGEUR_TILE;
+                Rect_source.h = HAUTEUR_TILE;
+                Rect_dest.h   = HAUTEUR_TILE;
+                for(i = 0 ; i < NOMBRE_BLOCS_LARGEUR; i++)
                 {
-                    case SDL_QUIT:
-                        continuer = 0;
-                        break;
+                    for(j = 0 ; j < NOMBRE_BLOCS_HAUTEUR; j++)
+                    {
+                        Rect_dest.x = i * LARGEUR_TILE;
+                        Rect_dest.y = j * HAUTEUR_TILE;
+                        Rect_source.x = (table[j][i] - '0') * LARGEUR_TILE;
+                        Rect_source.y = 0;
+                        SDL_RenderCopy(pRenderer, textuTil, &Rect_source, &Rect_dest);
+                    }
+                }
+            }
 
-                    case SDL_KEYDOWN:
-                        switch(event.key.keysym.sym)
-                        {
-                            case SDLK_ESCAPE:
-                                continuer = 0;
-                                break;
 
-                            case SDLK_d:
-                                if(PixelX < NOMBRE_BLOCS_LARGEUR)
-                                    PixelX = PixelX + 1;
-                                break;
+                //SDL_Window *window2 = NULL;
+                //SDL_Renderer *render2 = NULL;
+                SDL_Surface *tileset = NULL;
+                SDL_Texture *textuTil = NULL;
+                SDL_Event event;
+                int continuer = 1, tempsPrecedent = 0, tempsActuel = 0;
+                int statut = EXIT_FAILURE;
 
-                            case SDLK_q:
-                                if(PixelX > 0)
-                                    PixelX = PixelX - 1;
-                                break;
-                        }
-                        break;
+
+                if (SDL_Init(SDL_INIT_VIDEO) != 0)
+                {
+                    fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+                    //goto Quit;
+                }
+
+                pWindow = SDL_CreateWindow("TILESET_TEST", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                        LARGEUR_TILE*NOMBRE_BLOCS_LARGEUR, HAUTEUR_TILE*NOMBRE_BLOCS_HAUTEUR, SDL_WINDOW_SHOWN);
+                if (pWindow == NULL)
+                {
+                    fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+                    //goto Quit;
+                }
+
+                pRenderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED);
+                if (pRenderer == NULL)
+                {
+                    fprintf(stderr, "Erreur SDL_CreateRenderer : %s", SDL_GetError());
+                    //goto Quit;
+                }
+
+                //tileset = IMG_Load("tileset1.bmp");
+                tileset = IMG_Load("brick_tiles_1.bmp");
+                if (tileset == NULL)
+                {
+                    fprintf(stderr, "Erreur chargement image droite : %s", SDL_GetError());
+                    //goto Quit;
+                }
+
+                textuTil = SDL_CreateTextureFromSurface(pRenderer, tileset);
+                if (textuTil == NULL)
+                {
+                    fprintf(stderr, "Erreur SDL_CreateTexturetil : %s", SDL_GetError());
+                    //goto Quit;
+                }
+                SDL_FreeSurface(tileset);
+
+                Afficher(pRenderer, textuTil, table, NOMBRE_BLOCS_LARGEUR, NOMBRE_BLOCS_HAUTEUR);
+                while(continuer)
+                {
+                    SDL_PollEvent(&event);
+                    switch(event.type)
+                    {
+                        case SDL_QUIT:
+                            continuer = 0;
+                            break;
+
+                        case SDL_KEYDOWN:
+                            switch(event.key.keysym.sym)
+                            {
+                                case SDLK_ESCAPE:
+                                    continuer = 0;
+                                    break;
+                            }
+                            break;
+                    }
+
+                    tempsActuel = SDL_GetTicks();
+                    if (tempsActuel - tempsPrecedent > 30) /* si 30ms se sont écoulé depuis le dernier tour de la boucle
+                                                              plus le temps est petit plus il se deplace vite */
+                    {
+                        tempsPrecedent = tempsActuel; // le temps actuel devient le temps présent
+                    }
+                    else
+                    {
+                        SDL_Delay(30 - (tempsActuel - tempsPrecedent));
+                    }
+
+                    SDL_RenderPresent(pRenderer);
+
                 }
 
 
 
-                nTempsActuel = SDL_GetTicks();
-                if (nTempsActuel > nTempsPrecedent + nDeltaTime){
-
-                    Afficher(pRenderer, textuTil, table, NOMBRE_BLOCS_LARGEUR, NOMBRE_BLOCS_HAUTEUR,PixelX,PixelY);
-                    nTempsPrecedent = nTempsActuel;}
 
 
-
-                SDL_RenderPresent(pRenderer);
-            }
+            //------------------------------------------------------------------------------------------------//
 
 
-            SDL_DestroyTexture(textuTil);
+            //SDL_Delay(10000);
+
+            //SDL_DestroyTexture(Image);
+            //SDL_DestroyTexture(spriteGuy);
+            //SDL_FreeSurface(image);
+            //SDL_FreeSurface(surfaceSprite);
             SDL_DestroyRenderer(pRenderer); //On supprime tout ce qui à été créer
             SDL_DestroyWindow(pWindow);
             SDL_Quit();
-        }
+
+     }
 
     }
 
     return 0;
-}
-
-
-void Afficher(SDL_Renderer* pRenderer, SDL_Texture* textuTil, char** table, int nombre_blocs_largeur, int nombre_blocs_hauteur,int xBase, int yBase) {
-
-    SDL_RenderClear(pRenderer);
-    int i, j;
-    int BlocX, BlocY;
-    SDL_Rect Rect_dest;
-    SDL_Rect Rect_source;
-    BlocX = xBase / LARGEUR_TILE;
-    BlocY = yBase / HAUTEUR_TILE;
-
-    Rect_source.w = LARGEUR_TILE;
-    Rect_dest.w = LARGEUR_TILE;
-    Rect_source.h = HAUTEUR_TILE;
-    Rect_dest.h = HAUTEUR_TILE;
-    for(i = BlocX ; i < NOMBRE_BLOCS_LARGEUR + (BlocX+1); i++) {
-        for(j = BlocY ; j < NOMBRE_BLOCS_HAUTEUR + BlocY; j++) {
-            Rect_dest.x = i * LARGEUR_TILE - xBase;
-            Rect_dest.y = j * HAUTEUR_TILE - yBase;
-            Rect_source.x = (table[j][i] - '0') * LARGEUR_TILE;
-            Rect_source.y = 0;
-            SDL_RenderCopy(pRenderer, textuTil, &Rect_source, &Rect_dest);
-        }
-    }
 }
